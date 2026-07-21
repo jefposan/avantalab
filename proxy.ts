3058c02e86aa8b40293809ab7d9b41f05b3493c2
Jeff <jefposan@gmail.com>
2026-07-21 00:35:39 -0300

    Adiciona diagnóstico do host no proxy

--- proxy.ts
+++ proxy.ts
@@ -9,6 +9,15 @@ export function proxy(request: NextRequest) {
   const host = hostname.split(':')[0].toLowerCase();
   const pathname = request.nextUrl.pathname;
 
+  if (request.nextUrl.searchParams.get('debug-host') === '1') {
+  return NextResponse.json({
+    host: request.headers.get('host'),
+    forwardedHost: request.headers.get('x-forwarded-host'),
+    hostname: request.nextUrl.hostname,
+    url: request.url,
+  });
+}
+
   if (host === 'admin.avantalab.com.br' && pathname === '/') {
     const url = request.nextUrl.clone();
     url.pathname = '/admin';
