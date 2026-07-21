91a25690a71309f41d46aabc1073eb50d26f2cfb
Jeff <jefposan@gmail.com>
2026-07-21 00:46:39 -0300

    Finaliza subdominio admin

--- proxy.ts
+++ proxy.ts
@@ -1,24 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function proxy(request: NextRequest) {
-  const hostname =
+  const host = (
     request.headers.get('x-forwarded-host') ??
     request.headers.get('host') ??
-    '';
+    ''
+  )
+    .split(':')[0]
+    .toLowerCase();
 
-  const host = hostname.split(':')[0].toLowerCase();
-  const pathname = request.nextUrl.pathname;
-
-  if (request.nextUrl.searchParams.get('debug-host') === '1') {
-  return NextResponse.json({
-    host: request.headers.get('host'),
-    forwardedHost: request.headers.get('x-forwarded-host'),
-    hostname: request.nextUrl.hostname,
-    url: request.url,
-  });
-}
-
-  if (host === 'admin.avantalab.com.br' && pathname === '/') {
+  if (host === 'admin.avantalab.com.br' && request.nextUrl.pathname === '/') {
     const url = request.nextUrl.clone();
     url.pathname = '/admin';
 
